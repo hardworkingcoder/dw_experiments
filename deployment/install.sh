3e3aec23acbb67ff8f356e9e15ca0eb80ceacf53
Tim Clemans <tim@hardworkingcoder.com>
2017-07-09 22:07:52 +0000

    delete lines for production and fixed uwsgi bug

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,19 +1,13 @@
 # bash <(curl https://raw.githubusercontent.com/hardworkingcoder/dw_experiments/master/deployment/install.sh)
-#git clone git@github.com:hardworkingcoder/dw_experiments.git
 git clone git@github.com:hardworkingcoder/dw_experiments.git dw_experiments_dev
-#cd ~/dw_experiments
-#virtualenv --python=/usr/bin/python2 env
-#python -m pip install -r requirements.txt
 cd ~/dw_experiments_dev
 virtualenv --python=/usr/bin/python2 env
+source env/bin/activate
 python -m pip install -r requirements.txt
 sudo cp ~/dw_experiments/deployment/etc_nginx_sites_available_dw_experiments_dev_hardworkingcoder_com /etc/nginx/sites-available/dw_experiments_dev.hardworkingcoder.com
 sudo ln -s /etc/nginx/sites-available/dw_experiments_dev.hardworkingcoder.com /etc/nginx/sites-enabled/dw_experiments_dev.hardworkingcoder.com
-#sudo cp ~/dw_experiments/deployment/etc_nginx_sites_available_dw_experiments_hardworkingcoder_com /etc/nginx/sites-available/dw_experiments.hardworkingcoder.com
-#sudo ln -s /etc/nginx/sites-available/dw_experiments.hardworkingcoder.com /etc/nginx/sites-enabled/dw_experiments.hardworkingcoder.com
 sudo mkdir /etc/uwsgi/
 sudo mkdir /etc/uwsgi/sites
-#sudo cp ~/dw_experiments/deployment/dw_experiments.ini /etc/uwsgi/sites/dw_experiments.ini
 sudo cp ~/dw_experiments_dev/deployment/dw_experiments_dev.ini /etc/uwsgi/sites/dw_experiments_dev.ini
 sudo cp ~/dw_experiments_dev/deployment/uwsgi.service /etc/systemd/system/uwsgi.service
 sudo systemctl daemon-reload
