786ef62c4106bd4999fee433b1b65259e6935022
Tim Clemans <tim@hardworkingcoder.com>
2017-07-10 02:12:25 +0000

    trying to finish oauth

--- app.py
+++ app.py
@@ -34,17 +34,17 @@ from flask import jsonify
 
 @app.route('/login', strict_slashes=False)
 def login():
-    location = 'https://data.world/oauth/authorize?client_id=%s&redirect_uri=http://dw_experiments_dev.hardworkingcoder.com/dwoauthstep2&response_type=code' % app.config['DATADOTWORLD_CLIENT_ID']
+    location = 'https://data.world/oauth/authorize?client_id=%s&redirect_uri=http://dw_experiments_dev.hardworkingcoder.com/dwoauth&response_type=code' % app.config['DATADOTWORLD_CLIENT_ID']
     return flask.redirect(location, code=302)
 
-@app.route('/dwoauthstep2', strict_slashes=False)
-def dwoauthstep2():
+@app.route('/dwoauth', strict_slashes=False)
+def dwoauth():
      code = request.args.get('code')
-     response = requests.get('https://data.world/oauth/authorize?code=%s&client_id=%s&client_secret=%s&redirect_uri=http://dw_experiments_dev.hardworkingcoder.com/dwoauthstep3&grant_type=authorization_code' % (code, app.config['DATADOTWORLD_CLIENT_ID'], app.config['DATADOTWORLD_CLIENT_SECRET']))
+     response = requests.get('https://data.world/oauth/authorize?code=%s&client_id=%s&client_secret=%s&redirect_uri=http://dw_experiments_dev.hardworkingcoder.com/dwoauthstep2&grant_type=authorization_code' % (code, app.config['DATADOTWORLD_CLIENT_ID'], app.config['DATADOTWORLD_CLIENT_SECRET']))
      return jsonify(response.json())
 
 @app.route('/dwoauthstep3', strict_slashes=False)
-def dwoauthstep3():
+def dwoauthstep2():
      return jsonify(request.args)
     
     
