2763515824e0ea684d1eeda087e0fbe1ad36192c
Tim Clemans <tim@hardworkingcoder.com>
2017-07-11 15:25:00 +0000

    checkpoint for setting up auth using Flask-Login

--- app.py
+++ app.py
@@ -31,15 +31,23 @@ oauth = OAuth()
 from sqlalchemy.orm.attributes import flag_modified
 import uuid
 from flask import jsonify
+import requests
+from flask_login import LoginManager, UserMixin, login_user, logout_user,\
+    current_user
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(uuid):
+    return User.query.get(uuid)
 
 @app.route('/login', strict_slashes=False)
 def login():
-    location = 'https://data.world/oauth/authorize?client_id=%s&redirect_uri=http://dw_experiments_dev.hardworkingcoder.com/dwoauth&response_type=code' % app.config['DATADOTWORLD_CLIENT_ID']
+    location = 'https://data.world/oauth/authorize?client_id=%s&redirect_uri=https://dw_experiments_dev.hardworkingcoder.com/dwoauth&response_type=code' % app.config['DATADOTWORLD_CLIENT_ID']
     return flask.redirect(location, code=302)
 
-@app.route('/dwoauth', strict_slashes=False)
-def dwoauth():
-    code = request.args.get('code')
+def get_access_info(code):
     params = {
       'code': code,
       'client_id': app.config['DATADOTWORLD_CLIENT_ID'],
@@ -49,8 +57,55 @@ def dwoauth():
     params_as_str = '&'.join(['='.join(pair) for pair in params.items()])
     url = 'https://data.world/oauth/access_token?%s' % (params_as_str)
     response = requests.post(url)
-    return response.text
+    return response.json()
 
+def get_user_info(access_token):
+    url = "https://api.data.world/v0/user"
+    payload = "{}"
+    headers = {'authorization': 'Bearer <<%s>>' % (access_token)}
+    response = requests.request("GET", url, data=payload, headers=headers)
+    return response.json()
+
+def update_db_with_access_and_user_info(access_info, user_info):
+    user_exists = db.session.query(models.User.social_id).filter_by(social_id=user_info['id']).scalar() is not None
+    if user_exists:
+        user = models.User.query.filter_by(social_id=user_info['id']).first()
+        ddw_access_token = access_info['access_token']
+        ddw_token_expires_in = access_info['expires_in']
+        ddw_avatar_url = user_info['avatarUrl']
+        nickname = user_info['displayName']
+        ddw_user_updated = user_info['updated']
+        db.session.commit()
+    else:
+        user = models.User(ddw_access_token=access_info['access_token'], ddw_token_expires_in=access_info['expires_in'], ddw_avatar_url=user_info['avatarUrl'], nickname=user_info['displayName'], social_id=user_info['id'], ddw_user_created=user_info['created'], ddw_user_updated=user_info['updated'])
+        db.session.add(user)
+        db.session.commit()
+    return user
+
+def update_db_session_for_user(user):
+    session_row = models.Session(id = user.id)
+    db.session.add(session_row)
+    db.session.commit()
+    return session_row
+
+row2dict = lambda r: {c.name: str(getattr(r, c.name)) for c in r.__table__.columns}
+
+@app.route('/dwoauth', strict_slashes=False)
+def dwoauth():
+    access_info = get_access_info(request.args.get('code'))
+    user_info = get_user_info(access_info['access_token'])
+    user = update_db_with_access_and_user_info(access_info, user_info)
+    login_user(user, True)
+    return flask.redirect('/', code=302)
+
+@app.route('/logout', strict_slashes=False)
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/', strict_slashes=False)
+def index():
+    return send_from_directory('static', 'index.html')
 
 @failsafe
 def create_app():
--- models.py
+++ models.py
@@ -6,26 +6,20 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column
 import sqlalchemy
 from slugify import slugify
+from flask.ext.login import UserMixin
 
+class User(UserMixin, db.Model):
 
-class User(db.Model):
-    
     __tablename__ = "users"
-    
-    user_uuid = Column(UUID(as_uuid=True),
+
+    id = Column(UUID(as_uuid=True),
         server_default=sqlalchemy.text("uuid_generate_v4()"), primary_key=True)
     permissions_group = db.Column(db.String, default='user') # user, staff, admin
-    email = db.Column(db.String, unique=True)
-    name = db.Column(db.String)
-    datadotworld_access_token = db.Column(db.String) 
+    ddw_access_token = db.Column(db.String)
+    ddw_token_expires_in = db.Column(db.Integer)
+    ddw_avatar_url = db.Column(db.String)
+    nickname = db.Column(db.String)
+    social_id = db.Column(db.String, unique=True)
+    ddw_user_created = db.Column(db.Date)
+    ddw_user_updated = db.Column(db.Date)
     data = db.Column(postgresql.JSONB, nullable=True)
-
-class Session(db.Model):
-    
-    __tablename__ = "sessions"
-    
-    session_uuid = Column(UUID(as_uuid=True),
-        server_default=sqlalchemy.text("uuid_generate_v4()"), primary_key=True)
-    created = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
-    user_uuid = Column(UUID(as_uuid=True), db.ForeignKey("users.user_uuid"), nullable=True)
-    is_active = db.Column(db.Boolean, default=True, nullable=False)
